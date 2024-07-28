55ce9eb4524ed955efe30d434ea1c172d5f3b928
Farreeze <rizarcilla24@gmail.com>
2024-07-28 23:56:11 +0800

    added user registration|back&front

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\LibGender;
+use App\Models\libSuffixName;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -19,7 +21,9 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        $genders = LibGender::all();
+        $suffix_names = libSuffixName::all();
+        return view('auth.register', ['suffix_names' => $suffix_names, 'genders' => $genders]);
     }
 
     /**
@@ -30,13 +34,29 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'main_church' => ['boolean'],
+            'sub_church' => ['boolean'],
+            'user' => ['boolean'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'middle_name' => ['required', 'string', 'max:255'],
+            'gender' => ['required', 'exists:lib_genders,id'],
+            'address' => ['required'],
+            'mobile_number' => ['required', 'numeric', 'digits:11'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'main_church' => $request->main_church,
+            'sub_church' => $request->sub_church,
+            'user' => $request->user,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'middle_name' => $request->middle_name,
+            'gender' => $request->gender,
+            'address' => $request->address,
+            'mobile_number' => $request->mobile_number,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,12 +1,61 @@
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
-
+        {{-- form requirements --}}
+        <input type="hidden" name="main_church" value="0">
+        <input type="hidden" name="sub_church" value="0">
+        <input type="hidden" name="user" value="1">
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <x-input-label for="First Name" :value="__('First Name')" />
+            <x-text-input id="First Name" class="block mt-1 w-full" type="text" name="first_name" :value="old('First Name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('First Name')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="Last Name" :value="__('Last Name')" />
+            <x-text-input id="Last Name" class="block mt-1 w-full" type="text" name="last_name" :value="old('Last Name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('Last Name')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="Middle Name" :value="__('Middle Name')" />
+            <x-text-input id="Middle Name" class="block mt-1 w-full" type="text" name="middle_name" :value="old('Middle Name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('Middle Name')" class="mt-2" />
+        </div>
+
+        <div class="mt-4 text-gray-700">
+            <x-input-label for="Suffix Name" :value="__('Suffix Name')" />
+            <select class="w-full rounded-md border-gray-300 shadow-sm" name="suffix_name" id="">
+                <option value="" selected disabled>Select suffix name</option>
+                @foreach ($suffix_names as $suffix_name)
+                    <option value="{{ $suffix_name->id }}">{{ $suffix_name->desc }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        {{-- Gender --}}
+        <div class="mt-4 text-gray-700">
+            <x-input-label for="Gender" :value="__('Gender')" />
+            <select class="w-full rounded-md border-gray-300 shadow-sm" name="gender" id="">
+                <option value="" selected disabled>Select gender</option>
+                @foreach ($genders as $gender)
+                    <option value="{{ $gender->id }}">{{ $gender->desc }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        {{-- Address --}}
+        <div class="mt-4">
+            <x-input-label for="Address" :value="__('Address')" />
+            <textarea class="w-full rounded-md border-gray-300 shadow-sm" name="address" id="" cols="30" rows="5"></textarea>
+        </div>
+
+        {{-- Mobile Number --}}
+        <div class="mt-4">
+            <x-input-label for="Mobile Number" :value="__('Mobile Number')" />
+            <x-text-input id="Mobile Number" class="block mt-1 w-full" type="tel" name="mobile_number" :value="old('Mobile Number')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('Mobile Number')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
