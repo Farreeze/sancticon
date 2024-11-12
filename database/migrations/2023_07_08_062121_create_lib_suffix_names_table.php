a52212681b9a3926ae314812e8571d93eb07e8f0
Farreeze Arcilla <158267205+Farreeze@users.noreply.github.com>
2024-11-12 19:22:09 +0800

    adjusted suffix name seeder

--- database/migrations/2023_07_08_062121_create_lib_suffix_names_table.php
+++ database/migrations/2023_07_08_062121_create_lib_suffix_names_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,6 +17,19 @@ return new class extends Migration
             $table->string('desc');
             $table->timestamps();
         });
+
+        DB::table('lib_suffix_names')->insert([
+            ['id' => '1',     'desc' => 'Jr.'],
+            ['id' => '2',     'desc' => 'Sr.'],
+            ['id' => '3',     'desc' => 'II'],
+            ['id' => '4',     'desc' => 'III'],
+            ['id' => '5',     'desc' => 'IV'],
+            ['id' => '6',     'desc' => 'V'],
+            ['id' => '7',     'desc' => 'VI'],
+            ['id' => '8',     'desc' => 'VII'],
+            ['id' => '9',     'desc' => 'VIII'],
+            ['id' => '10',    'desc' => 'N/A'],
+        ]);
     }
 
     /**
